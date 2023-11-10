9be13f896d5c6951d6cbbe44e584dfd38e6fe5da
Zachary Kipping <zachary.kipping@oasisdigital.com>
2023-11-10 11:15:09 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [NgIf]
 })
 export class AppComponent {
   n = 0;
